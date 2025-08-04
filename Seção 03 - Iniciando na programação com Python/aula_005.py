4e3beb245456b032540fe6bf5d62406bf794972d
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2025-08-04 06:47:36 -0300

    Tipo bool - Introdução aos tipos de dados

---
+++ Seção 03 - Iniciando na programação com Python/aula_005.py
@@ -0,0 +1,13 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa, só existem duas respostas possíveis:
+# sim (True) ou não (False).
+# Usa-se os operadores lógicos para "questionar" os valores.
+
+booleano_1 = 10 == 10
+booleano_2 = 10 == 11
+tipo = type(booleano_1)
+
+print(booleano_1) # Saída: True
+print(booleano_2) # Saída: False
+print(tipo) # Saída: <class 'bool'> 
+
