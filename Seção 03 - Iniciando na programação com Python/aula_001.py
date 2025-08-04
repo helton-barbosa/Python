934c7fcf80da80d6f1ba6690ad13dc7caa40b5d1
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2025-08-03 21:53:51 -0300

    Comentários e Docstrings

---
+++ Seção 03 - Iniciando na programação com Python/aula_001.py
@@ -0,0 +1,24 @@
+# Permite escrever um comentário em uma linha
+print(123) # Comentário à frente do código
+# Comentário abaixo do código
+
+"""
+Docstring é uma string literal que serve como documentação para um módulo,
+função, classe ou método.
+Ela é a primeira declaração em um desses blocos de código e é delimitada por
+aspas triplas ou simples.
+"""
+
+def somar(a, b):
+    """
+    Esta função retorna a soma de dois números.
+    
+    Args:
+        a (int or float): O primeiro número.
+        b (int or float): O segundo número.
+
+    Returns:
+        int or float: A soma de 'a' e 'b'.
+    """
+    return a + b
+
