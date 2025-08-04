bec83ea2a2c9ef6eb11dcaf9d44177a78a59697e
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2025-08-03 22:26:52 -0300

    Tipo str(strng) - Introdução aos tipos de dados

---
+++ Seção 03 - Iniciando na programação com Python/aula_003.py
@@ -0,0 +1,19 @@
+# str -> String -> texto
+# Strings são textos dentro de aspas
+
+# Aspas simples
+nome_1 = 'Helton'
+print(nome_1)
+
+# Aspas duplas
+nome_2 = "Helton"
+print(nome_2)
+
+# Escape
+nome_3 = "Helton\'s"
+print(nome_3)
+
+# r
+nome_4 = r"Helton \"Lampião\""
+print(nome_4)
+
