ee4d131babce5a802ad24a9a5bfc8009adc109d4
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2025-08-04 21:17:54 -0300

    Introdução às variáveis em Python

---
+++ Seção 03 - Iniciando na programação com Python/aula_007.py
@@ -0,0 +1,9 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode ter número e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor
+# a um nome (variável).
+# É recomendável utilizar nomes de variáveis que indiquem o que é gravado
+# Uso: nome_variavel = expressão
+
+nome_completo = 'Helton Barbosa Santos Ferreira'
+print(nome_completo) # Saída: Helton Barbosa Santos Ferreira
