3b2a33f7ce1b0bb459083c46bfc7a48f27daaca2
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2025-08-04 21:05:56 -0300

    Coerção de Tipos

---
+++ Seção 03 - Iniciando na programação com Python/aula_006.py
@@ -0,0 +1,20 @@
+# conversão de tipos, coerção, type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+
+# soma_incorreta = '1' + 1
+# print(soma_incorreta)
+
+soma_correta = int('1') + 1
+print(soma_correta)
+
+soma_float = float('10') + 10
+print(soma_float)
+
+caracteres = str(1) + '2'
+print(caracteres)
+
+booleano = bool('1')
+print(booleano)
+
