41d60f1898861780493c7e7d8b872b5a0cf3c856
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2025-08-05 06:26:50 -0300

    Introdução aos operadores aritméticos

---
+++ Seção 03 - Iniciando na programação com Python/aula_009.py
@@ -0,0 +1,21 @@
+adicao = 10 + 10
+print(f'Adição: {adicao}')
+
+subtracao = 10 - 5
+print(f'Subtração: {subtracao}')
+
+multiplicacao = 10 * 10
+print(f'Multiplicação: {multiplicacao}')
+
+divisao = 10 / 2.2
+print(f'Divisão: {divisao}')
+
+divisao_inteira = 10 // 3 # Arredonda para o número inteiro mais próximo
+print(f'Divisão Inteira: {divisao_inteira}')
+
+exponenciacao = 2 ** 10 # Potência
+print(f'Exponenciação: {exponenciacao}')
+
+modulo = 10 % 3 # Resto da divisão
+print(f'Módulo: {modulo}')
+
