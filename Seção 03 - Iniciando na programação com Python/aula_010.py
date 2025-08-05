c7663749f5286c153dc335ce5655db0c7bbdf3b8
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2025-08-05 06:35:02 -0300

    Concatenação (+) e repetição (*)

---
+++ Seção 03 - Iniciando na programação com Python/aula_010.py
@@ -0,0 +1,8 @@
+concatenacao = 'A' + 'B' + 'C' # + faz a concatenação
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10 # * faz a repetição
+tres_vezes_helton = 3 * 'Helton' # * faz a repetição
+print(a_dez_vezes)
+print(tres_vezes_helton)
+
