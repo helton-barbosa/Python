7d6e4277d8e2797dbb7169b03fa2af8216ed841f
Helton Barbosa Santos Ferreira <helton.ferreira@atualcargas.com.br>
2025-08-06 17:38:40 -0300

    Função input

---
+++ Seção 03 - Iniciando na programação com Python/aula_015.py
@@ -0,0 +1,10 @@
+nome = input('Digite o nome: ')
+idade = input('Digite idade: ')
+print(f'{nome=} | {idade=}')
+print(f'{type(nome)} {type(idade)}') 
+
+idade_int = int(idade)
+
+
+print(f'{nome} tem {idade_int} anos.')
+
